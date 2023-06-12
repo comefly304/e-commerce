b42fb64a8ffbea90ca7af5d394a142a906575bc3
Kn Sachin <hi636112hello@gmail.com>
2023-06-12 14:49:38 +0530

    main code

---
+++ models/orderModel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        type: mongoose.ObjectId,
+        ref: "Products",
+      },
+    ],
+    payment: {},
+    buyer: {
+      type: mongoose.ObjectId,
+      ref: "users",
+    },
+    dogstatus: {
+      type: String,
+      default: "No",
+      enum: ["Yes", "No"],
+    },
+    ordertype: {
+      type: String,
+      default: "Wait For me to collect the order",
+      enum: [ "Wait For me to collect the order","Keep the order near my door step","Call me i will come right after"],
+    },
+    status: {
+      type: String,
+      default: "Not Process",
+      enum: ["Not Process", "Processing", "Shipped", "deliverd", "cancel"], //selelct box
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Order", orderSchema);
