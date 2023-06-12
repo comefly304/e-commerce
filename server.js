2dccb682f39a73edf37d95d9fa7650506bc91ce4
Kn Sachin <hi636112hello@gmail.com>
2023-06-12 14:58:40 +0530

    server modfied

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const app = express();
 
 //middelwares
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors());
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 app.use(express.json());
